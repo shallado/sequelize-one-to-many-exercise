d1ec3bc5f8e08f376dd862c9cd56a17bdf2d160f
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-16 22:49:59 -0700

    Setup method for finding a tutorial by id

--- controllers/tutorial.js
+++ controllers/tutorial.js
@@ -8,3 +8,15 @@ exports.createTutorial = (tutorialInfo) => {
     })
     .catch((err) => console.log('Unable to create tutorial', err));
 };
+
+exports.findTutorial = (tutorialId) => {
+  return Tutorial.findByPk(tutorialId, { include: ['comments'] })
+    .then((tutorial) => {
+      if (!tutorial) {
+        console.log('No tutorial found');
+      }
+
+      return console.log(JSON.stringify(tutorial, null, 2));
+    })
+    .catch((err) => console.log(err));
+};
